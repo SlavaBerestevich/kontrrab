557bc2a0937d00562da66db92aa2b0f55e30522e
Slava <berestevich1998@mail.rum>
2024-02-27 18:29:12 +0300

    2 commit

--- kontrrab.py
+++ kontrrab.py
@@ -1,6 +1,12 @@
-def filter_strings(array):
-    new_array = []
-    for string in array:
+def filter_strings():
+    size = int(input("Введите размер массива строк: "))
+    strings = []
+    for i in range(size):
+        string = input(f"Введите строку {i+1}: ")
+        strings.append(string)
+    new_strings = []
+    for string in strings:
         if len(string) <= 3:
-            new_array.append(string)
-    return new_array
+            new_strings.append(string)
+    
+    return new_strings
